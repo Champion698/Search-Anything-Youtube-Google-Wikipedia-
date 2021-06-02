2de3b4ea83a0f8c8e070e1b66fb1228d1e48ac0d
Champion698 <85236043+Champion698@users.noreply.github.com>
2021-06-02 18:49:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import pywhatkit as kit
+
+play=input ("What would you like to play: ")
+search=input ("What would you like to search: ")
+info=input ("What would you like to know about: ")
+
+kit.playonyt(play)
+kit.search(search)
+kit.info(info)
